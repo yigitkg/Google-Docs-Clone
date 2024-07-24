6fd400ce92615e93afaa61a389e922fa6a9f82f1
yigitkg <yigitkg@gmail.com>
2024-07-24 10:44:23 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import { Metadata } from "next";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your go-to colloborative editor",
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
